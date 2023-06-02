3c8143de00e799de15f91358960c47fb322e2943
chadjcampbell <chadjcampbell@gmail.com>
2023-06-02 11:56:03 -0400

    finish convert to express routing

--- expressServer.js
+++ expressServer.js
@@ -3,7 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.end("./views/index.html");
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+app.get("/contact", (req, res) => {
+  res.sendFile("./views/contact.html", { root: __dirname });
+});
+app.get("/*", (req, res) => {
+  res.sendFile("./views/404.html", { root: __dirname });
 });
 
 app.listen(port, () => {
